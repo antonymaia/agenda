35bbee328bd9d36745a6583c06a90efa8c11c74d
Antony Maia <antonymaiati@gmail.com>
2021-12-10 22:53:51 -0300

    Configurando CORS

--- src/main/java/br/unicap/agenda/controller/EventoController.java
+++ src/main/java/br/unicap/agenda/controller/EventoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/evento")
 public class EventoController {
